d19e04c93ff780f994ee907b12b9acec02cef00d
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-05-09 22:36:09 -0500

    Documentation comments in buy_optimizer.

--- magiccardbuyer/buy_optimizer.py
+++ magiccardbuyer/buy_optimizer.py
@@ -1,14 +1,39 @@
+"""Optimizer to solve the cheapest solution for buying goods from vendors.
+
+In particular, we are looking to solve the problem where we want a certain number of each distinct
+type of good. Each good is available from some number of different vendors, each of whom has some
+quantity for offer at some price. Furthermore, and this is the tricky part, each vendor is unwilling
+to sell to us unless we meet a minimum threshold for our entire purchase from that vendor. (All of
+these quantities are integers.)
+
+If it weren't for the minimum threshold, we would just greedily buy from the cheapest vendor for
+each good. In order to satisfy the minimum threshold problem, though, we need to solve this as a
+mixed-integer linear programming problem, minimizing an objective that is linear in our variables,
+and with constraints that are linear as well. (You'll see below that we come up with a clever hack,
+suggested to me by Michael Kaye, defining a 0 or 1 variable for whether we purchase from a vendor,
+in order to linearize the minimum threshold requirement.)
+
+We use Google's OR Tools library to solve the MILP problem once we have expressed it.
+"""
+
+
 from dataclasses import dataclass, field
 from itertools import groupby
 from typing import List, Dict, Any
 from ortools.linear_solver import pywraplp
 
-def groupToDict(inputList: List[Any], by) -> Dict[Any, Any]:
+def groupToDict(inputList: List[Any], by) -> Dict[Any, List[Any]]:
+	"""Helper method for turning a list into a dict, grouping by a provided function.
+
+	The equivalent of Java or Kotlin's groupBy."""
 	sortedList = sorted(inputList, key=by)
 	return {key: list(values) for key, values in groupby(sortedList, by)}
 
 @dataclass
 class PurchaseOption:
+	"""A single option for purchasing a particular good from a particular vendor.
+
+	Goods and vendors are correlated by their string name."""
 	good: str
 	vendor: str
 	availableQuantity: int 
@@ -16,6 +41,9 @@ class PurchaseOption:
 
 @dataclass
 class VendorProblem:
+	"""An optimization problem to be solved.
+
+	minimumRequiredPurchase -- this is for each vendor."""
 	goodQuantitiesSought: Dict[str, int]
 	purchaseOptions: List[PurchaseOption]
 	minimumRequiredPurchase: int
@@ -29,11 +57,16 @@ class VendorProblem:
 
 @dataclass
 class PurchaseToMake:
+	"""As part of a result, how many of the good should be purchased from this option."""
 	quantity: int
 	option: PurchaseOption
 
 @dataclass
 class VendorSolution:
+	"""A total solution.
+
+	purchasesToMake -- this contains only purchases that should be made; i.e., quantity > 0.
+	totalCost -- this is theoretically derivable from purchasesToMake but is here for convenience."""
 	purchasesToMake: List[PurchaseToMake]
 	totalCost: int
 
@@ -43,8 +76,9 @@ class UnsolvableError(Exception):
 
 class BuyOptimizer:
 	def __init__(self):
+		# This large number is used to enforce the "buy flag". If you're ever trying to buy
+		# more total goods than this, the optimizer will break down.
 		self.LARGE_NUM = 1000000
-		self.MINIMUM_SPEND = 200
 
 	def solve(self, problem: VendorProblem) -> VendorSolution:
 		solver = pywraplp.Solver('SolveIntegerProblem',
