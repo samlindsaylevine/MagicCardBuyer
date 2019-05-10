cc186838b1a6ce4c1b85eff3c2403f6303aed0cc
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-05-09 22:20:42 -0500

    buy_optimizer working correctly, with unit tests.

--- magiccardbuyer/buy_optimizer.py
+++ magiccardbuyer/buy_optimizer.py
@@ -27,13 +27,26 @@ class VendorProblem:
 		self.optionsByGood = groupToDict(self.purchaseOptions, lambda option: option.good)
 		self.optionsByVendor = groupToDict(self.purchaseOptions, lambda option: option.vendor)
 
+@dataclass
+class PurchaseToMake:
+	quantity: int
+	option: PurchaseOption
+
+@dataclass
+class VendorSolution:
+	purchasesToMake: List[PurchaseToMake]
+	totalCost: int
+
+class UnsolvableError(Exception):
+	def __init__(self, message):
+		super().__init__(message)
 
 class BuyOptimizer:
 	def __init__(self):
 		self.LARGE_NUM = 1000000
 		self.MINIMUM_SPEND = 200
 
-	def solve(self, problem: VendorProblem):
+	def solve(self, problem: VendorProblem) -> VendorSolution:
 		solver = pywraplp.Solver('SolveIntegerProblem',
 			pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
@@ -52,66 +65,43 @@ class BuyOptimizer:
 		# We need the desired amount of each good.
 		for (good, quantity) in problem.goodQuantitiesSought.items():
 			desiredAmount = solver.Constraint(quantity, quantity)
-			print(f"Desiring {quantity} {good}")
+			if good not in variablesByGood:
+				raise UnsolvableError(f"Desire {good} but it is unavailable")
 			for (variable, option) in variablesByGood[good]:
 				desiredAmount.SetCoefficient(variable, 1)
-				print(f"  Including {option.vendor} purchase option")
 
 		# Don't buy anything from a merchant unless its flag is set.
 		# (quantity1 + quantity2 + ....) - (LARGE_NUM) * buyFlag <= 0
 		# This constraint is a bit of a hack in order to maintain the linearity of the problem. We pick an arbitrarily
 		# large coefficient on the buy flag.
 		for vendor in buyFlagsByVendor.keys():
-			print(f"Requring buy flag to be set for {vendor}")
 			buyAnything = solver.Constraint(-solver.infinity(), 0)
 			buyAnything.SetCoefficient(buyFlagsByVendor[vendor], -self.LARGE_NUM)
 			for variable in variablesByVendor[vendor]:
 				buyAnything.SetCoefficient(variable[0], 1)
-				print(f"  Including {variable[1].good}")
 
 		# If we do buy anything from a merchant, we need to spend at least the minimum amount.
 		# (MINIMUM_SPEND) * buyFlag - quantity1 * cost1 - quantity2 * cost2 - ... <= 0
 		# Here is where the buyFlag lets us maintain linearity.
 		for vendor in buyFlagsByVendor.keys():
-			print(f"Requring minimum spend for {vendor}")
 			minimumSpend = solver.Constraint(-solver.infinity(), 0)
 			minimumSpend.SetCoefficient(buyFlagsByVendor[vendor], problem.minimumRequiredPurchase)
 			for variable in variablesByVendor[vendor]:
 				minimumSpend.SetCoefficient(variable[0], -variable[1].price)
-				print(f"  Including {variable[1].good}")
 
-		print("Defining total cost")
 		totalCost = solver.Objective()
 		for variable in quantityVariables:
 			totalCost.SetCoefficient(variable[0], variable[1].price)
-			print(f"  Including {variable[1].vendor} {variable[1].good} {variable[1].price}")
 		totalCost.SetMinimization()
 
 		result_status = solver.Solve()
-		print('Number of variables =', solver.NumVariables())
-		print('Number of constraints =', solver.NumConstraints())
-		print("Solution completed")
-		print(f"Result status: {result_status}")
-		print(f"Verify solution: {solver.VerifySolution(1e-7, True)}")
+		if (result_status != pywraplp.Solver.OPTIMAL):
+			raise UnsolvableError(f"Vendor problem was not mathematically solveable; returned status {result_status}")
 
-		for variable in quantityVariables:
-			print(f"{variable[0].name()} = {variable[0].solution_value()}")
-		print(f"total cost: {solver.Objective().Value()}")
+		if (not solver.VerifySolution(1e-7, True)):
+			raise UnsolvableError("Solution could not be verified as legitimate")
 
-if __name__ == "__main__":
-	problem = VendorProblem(minimumRequiredPurchase = 200,
-			goodQuantitiesSought = { "apple": 1, "banana": 1, "coconut": 1, "date": 1},
-			purchaseOptions = [PurchaseOption("apple", "Merchant1", 100, 112),
-				PurchaseOption("banana", "Merchant1", 100, 95),
-				PurchaseOption("coconut", "Merchant1", 15, 17),
-				PurchaseOption("date", "Merchant1", 1, 1000),
-				PurchaseOption("apple", "Merchant2", 1, 100000),
-				PurchaseOption("banana", "Merchant2", 1, 15),
-				PurchaseOption("coconut", "Merchant2", 1, 10),
-				PurchaseOption("date", "Merchant2", 1, 200),
-				PurchaseOption("banana", "Merchant3", 1, 1)])
-
-	print(problem.optionsByGood)
-	print(problem.optionsByVendor)
-
-	BuyOptimizer().solve(problem)
+		purchasesToMake = [PurchaseToMake(variable[0].solution_value(), variable[1]) 
+			for variable in quantityVariables if variable[0].solution_value() > 0]
+		totalCost = solver.Objective().Value()
+		return VendorSolution(purchasesToMake, totalCost)
