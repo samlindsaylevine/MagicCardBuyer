d405a03263ed04fa3fd27b24a797aa5101354219
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-11-12 16:20:01 -0600

    Tidied up formatting in buy_optimizer.

--- magiccardbuyer/buy_optimizer.py
+++ magiccardbuyer/buy_optimizer.py
@@ -18,17 +18,20 @@ We use Google's OR Tools library to solve the MILP problem once we have expresse
 
 from collections import defaultdict
 from dataclasses import dataclass, field
-from itertools import groupby
-from typing import List, Dict, Any
+from typing import List, Dict, Any, TypeVar, Callable
+
 from ortools.linear_solver import pywraplp
 
+S = TypeVar('S')
+T = TypeVar('T')
 
-def groupToDict(inputList: List[Any], by) -> Dict[Any, List[Any]]:
+
+def group_to_dict(input_list: List[T], by: Callable[[T], S]) -> Dict[S, T]:
     """Helper method for turning a list into a dict, grouping by a provided function.
 
-	The equivalent of Java or Kotlin's groupBy."""
+    The equivalent of Java or Kotlin's groupBy."""
     output = defaultdict(list)
-    for item in inputList:
+    for item in input_list:
         output[by(item)].append(item)
     return {key: value for key, value in output.items()}
 
@@ -37,16 +40,17 @@ def groupToDict(inputList: List[Any], by) -> Dict[Any, List[Any]]:
 class PurchaseOption:
     """A single option for purchasing a particular good from a particular vendor.
 
-	Goods can be anything but must have a string repr and an equals, and be hashable (Strings, dataclasses...
-	Note that if you use a dataclass it must be hashable - i.e., frozen or unsafe hash.)
+    Goods can be anything but must have a string repr and an equals, and be hashable (Strings, dataclasses...
+    Note that if you use a dataclass it must be hashable - i.e., frozen or unsafe hash.)
 
-	Vendors are correlated by their string name."""
+    Vendors are correlated by their string name."""
     good: Any
     vendor: str
     availableQuantity: int
     price: int
 
     """This unique key identifies the option. It must not collide with any other option in the problem."""
+
     def key(self):
         return f"{self.vendor}_{self.good}"
 
@@ -55,7 +59,7 @@ class PurchaseOption:
 class VendorProblem:
     """An optimization problem to be solved.
 
-	minimumRequiredPurchase -- this is for each vendor."""
+    minimumRequiredPurchase -- this is for each vendor."""
     goodQuantitiesSought: Dict[Any, int]
     purchaseOptions: List[PurchaseOption]
     minimumRequiredPurchase: int
@@ -64,8 +68,8 @@ class VendorProblem:
     optionsByVendor: Dict[str, PurchaseOption] = field(init=False)
 
     def __post_init__(self):
-        self.optionsByGood = groupToDict(self.purchaseOptions, lambda option: option.good)
-        self.optionsByVendor = groupToDict(self.purchaseOptions, lambda option: option.vendor)
+        self.optionsByGood = group_to_dict(self.purchaseOptions, lambda option: option.good)
+        self.optionsByVendor = group_to_dict(self.purchaseOptions, lambda option: option.vendor)
 
 
 @dataclass
@@ -79,8 +83,8 @@ class PurchaseToMake:
 class VendorSolution:
     """A total solution.
 
-	purchasesToMake -- this contains only purchases that should be made; i.e., quantity > 0.
-	totalCost -- this is theoretically derivable from purchasesToMake but is here for convenience."""
+    purchasesToMake -- this contains only purchases that should be made; i.e., quantity > 0.
+    totalCost -- this is theoretically derivable from purchasesToMake but is here for convenience."""
     purchasesToMake: List[PurchaseToMake]
     totalCost: int
 
@@ -101,57 +105,58 @@ class BuyOptimizer:
                                  pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
         # A list of (variable, option) pairs.
-        quantityVariables = [(solver.IntVar(0, option.availableQuantity, f"{option.key()}_quantity"),
+        quantity_variables = [(solver.IntVar(0, option.availableQuantity, f"{option.key()}_quantity"),
                               option) for option in problem.purchaseOptions]
 
-        variablesByGood = groupToDict(quantityVariables, lambda variable: variable[1].good)
-        variablesByVendor = groupToDict(quantityVariables, lambda variable: variable[1].vendor)
+        variables_by_good = group_to_dict(quantity_variables, lambda v: v[1].good)
+        variables_by_vendor = group_to_dict(quantity_variables, lambda v: v[1].vendor)
 
         # We define a variable for each vendor that is whether we are purchasing anything for that vendor: the
         # "buy flag". The existence of this flag lets us use the "hacky" constraints below to maintain linearity
         # of the problem.
-        buyFlagsByVendor = {vendor: solver.IntVar(0, 1, f"buyFlag_{vendor}") for vendor in variablesByVendor.keys()}
+        buy_flags_by_vendor = {vendor: solver.IntVar(0, 1, f"buyFlag_{vendor}")
+                               for vendor in variables_by_vendor.keys()}
 
         # We need the desired amount of each good.
         for (good, quantity) in problem.goodQuantitiesSought.items():
-            desiredAmount = solver.Constraint(quantity, quantity)
-            if good not in variablesByGood:
+            desired_amount = solver.Constraint(quantity, quantity)
+            if good not in variables_by_good:
                 raise UnsolvableError(f"Desire {good} but it is unavailable")
-            for (variable, option) in variablesByGood[good]:
-                desiredAmount.SetCoefficient(variable, 1)
+            for (variable, option) in variables_by_good[good]:
+                desired_amount.SetCoefficient(variable, 1)
 
         # Don't buy anything from a merchant unless its flag is set.
         # (quantity1 + quantity2 + ....) - (LARGE_NUM) * buyFlag <= 0
         # This constraint is a bit of a hack in order to maintain the linearity of the problem. We pick an arbitrarily
         # large coefficient on the buy flag.
-        for vendor in buyFlagsByVendor.keys():
-            buyAnything = solver.Constraint(-solver.infinity(), 0)
-            buyAnything.SetCoefficient(buyFlagsByVendor[vendor], -self.LARGE_NUM)
-            for variable in variablesByVendor[vendor]:
-                buyAnything.SetCoefficient(variable[0], 1)
+        for vendor in buy_flags_by_vendor.keys():
+            buy_anything = solver.Constraint(-solver.infinity(), 0)
+            buy_anything.SetCoefficient(buy_flags_by_vendor[vendor], -self.LARGE_NUM)
+            for variable in variables_by_vendor[vendor]:
+                buy_anything.SetCoefficient(variable[0], 1)
 
         # If we do buy anything from a merchant, we need to spend at least the minimum amount.
         # (MINIMUM_SPEND) * buyFlag - quantity1 * cost1 - quantity2 * cost2 - ... <= 0
         # Here is where the buyFlag lets us maintain linearity.
-        for vendor in buyFlagsByVendor.keys():
-            minimumSpend = solver.Constraint(-solver.infinity(), 0)
-            minimumSpend.SetCoefficient(buyFlagsByVendor[vendor], problem.minimumRequiredPurchase)
-            for variable in variablesByVendor[vendor]:
-                minimumSpend.SetCoefficient(variable[0], -variable[1].price)
+        for vendor in buy_flags_by_vendor.keys():
+            minimum_spend = solver.Constraint(-solver.infinity(), 0)
+            minimum_spend.SetCoefficient(buy_flags_by_vendor[vendor], problem.minimumRequiredPurchase)
+            for variable in variables_by_vendor[vendor]:
+                minimum_spend.SetCoefficient(variable[0], -variable[1].price)
 
-        totalCost = solver.Objective()
-        for variable in quantityVariables:
-            totalCost.SetCoefficient(variable[0], variable[1].price)
-        totalCost.SetMinimization()
+        total_cost = solver.Objective()
+        for variable in quantity_variables:
+            total_cost.SetCoefficient(variable[0], variable[1].price)
+        total_cost.SetMinimization()
 
         result_status = solver.Solve()
-        if (result_status != pywraplp.Solver.OPTIMAL):
+        if result_status != pywraplp.Solver.OPTIMAL:
             raise UnsolvableError(f"Vendor problem was not mathematically solveable; returned status {result_status}")
 
-        if (not solver.VerifySolution(1e-7, True)):
+        if not solver.VerifySolution(1e-7, True):
             raise UnsolvableError("Solution could not be verified as legitimate")
 
-        purchasesToMake = [PurchaseToMake(variable[0].solution_value(), variable[1])
-                           for variable in quantityVariables if variable[0].solution_value() > 0]
-        totalCost = solver.Objective().Value()
-        return VendorSolution(purchasesToMake, totalCost)
+        purchases_to_make = [PurchaseToMake(variable[0].solution_value(), variable[1])
+                           for variable in quantity_variables if variable[0].solution_value() > 0]
+        total_cost = solver.Objective().Value()
+        return VendorSolution(purchases_to_make, total_cost)
